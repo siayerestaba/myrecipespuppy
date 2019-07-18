43fb224cfb00e93c6639a6ea6d15f7587c8cd11e
Ilia <ilia.berlana@gmail.com>
2019-07-18 12:13:10 +0200

    SearchRecipe usecase and test Changed RecipeRepository: return Either and become a suspend fun

--- domain/src/main/java/com/iliaberlana/myrecipepuppy/domain/data/RecipeRepository.kt
+++ domain/src/main/java/com/iliaberlana/myrecipepuppy/domain/data/RecipeRepository.kt
@@ -1,9 +1,11 @@
 package com.iliaberlana.myrecipepuppy.domain.data
 
+import arrow.core.Either
 import com.iliaberlana.myrecipepuppy.domain.entities.Recipe
+import com.iliaberlana.myrecipepuppy.domain.exception.DomainError
 
 interface RecipeRepository {
-    fun searchRecipes(ingredients: String): List<Recipe>
+    suspend fun searchRecipes(ingredients: String): Either<DomainError, List<Recipe>>
 
     fun showRecipe(recipe: Recipe)
 }
