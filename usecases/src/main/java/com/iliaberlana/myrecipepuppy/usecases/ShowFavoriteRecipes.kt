f84bbbdce318afb8c6fccb059369b9cf279bd8c2
Ilia <ilia.berlana@gmail.com>
2019-07-20 21:15:58 +0200

    Usecases moved to correct package

---
+++ usecases/src/main/java/com/iliaberlana/myrecipepuppy/usecases/ShowFavoriteRecipes.kt
@@ -0,0 +1,12 @@
+package com.iliaberlana.myrecipespuppy.usecases
+
+import arrow.core.Either
+import com.iliaberlana.myrecipepuppy.domain.data.FavoriteRecipeRepository
+import com.iliaberlana.myrecipepuppy.domain.entities.Recipe
+import com.iliaberlana.myrecipepuppy.domain.exception.RoomError
+
+class ShowFavoriteRecipes(
+    private val favoriteRecipeRepository: FavoriteRecipeRepository
+) {
+    suspend operator fun invoke(): Either<RoomError, List<Recipe>> = favoriteRecipeRepository.getFavorites()
+}
---
+++ usecases/src/test/java/com/iliaberlana/myrecipepuppy/usecases/ShowFavoriteRecipesTest.kt
@@ -0,0 +1,21 @@
+import com.iliaberlana.myrecipepuppy.domain.data.FavoriteRecipeRepository
+import com.iliaberlana.myrecipepuppy.domain.data.RecipeRepository
+import com.iliaberlana.myrecipespuppy.usecases.SearchRecipes
+import com.iliaberlana.myrecipespuppy.usecases.ShowFavoriteRecipes
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class ShowFavoriteRecipesTest {
+
+    @Test
+    fun `call FavoriteRecipeRepository when execute showFavorites`() = runBlocking {
+        val favRecipeRepository = mockk<FavoriteRecipeRepository>(relaxed = true)
+        val showFavoriteRecipes = ShowFavoriteRecipes(favRecipeRepository)
+
+        showFavoriteRecipes()
+
+        coVerify { favRecipeRepository.getFavorites() }
+    }
+}
