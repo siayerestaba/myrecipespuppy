43cc847c1e786ce88665895ec5a1b872ef61ac97
Ilia <ilia.berlana@gmail.com>
2019-07-21 12:08:51 +0200

    Refactor: delete baseActivity

--- app/src/main/java/com/iliaberlana/myrecipepuppy/ui/listrecipe/ListRecipeView.kt
+++
@@ -1,9 +0,0 @@
-package com.iliaberlana.myrecipepuppy.ui.listrecipe
-
-import com.iliaberlana.myrecipepuppy.ui.commons.BaseView
-import com.iliaberlana.myrecipepuppy.ui.model.RecipeUI
-
-interface ListRecipeView: BaseView {
-    fun listRecipes(recipes: List<RecipeUI>)
-    fun cleanRecipes()
-}
