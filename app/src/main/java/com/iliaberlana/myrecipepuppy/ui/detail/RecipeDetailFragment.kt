ae50b70d4c47994c991b5bd71129e045e6c65ed2
Ilia <ilia.berlana@gmail.com>
2019-07-21 14:21:27 +0200

    Implemented WebView

--- app/src/main/java/com/iliaberlana/myrecipepuppy/ui/detail/RecipeDetailFragment.kt
+++ app/src/main/java/com/iliaberlana/myrecipepuppy/ui/detail/RecipeDetailFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.iliaberlana.myrecipepuppy.R
 import com.iliaberlana.myrecipepuppy.ui.commons.logDebug
 import kotlinx.android.synthetic.main.recipe_detail.view.*
 
 class RecipeDetailFragment : Fragment() {
     private lateinit var linkRecipe: String
+    private lateinit var webview: WebView
 
     companion object {
         const val RECIPE_LINK = "recipe_link"
@@ -29,7 +32,22 @@ class RecipeDetailFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.recipe_detail, container, false)
-        rootView.recipe_detail.text = linkRecipe
+
+        webview = rootView.webPage as WebView
+        webview.settings.builtInZoomControls = true
+        webview.settings.javaScriptEnabled = true
+        webview.settings.javaScriptCanOpenWindowsAutomatically = true
+        webview.settings.allowFileAccess = true
+        webview.settings.domStorageEnabled = true
+
+        webview.loadUrl(linkRecipe)
+        webview.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                view.loadUrl(url)
+                return true
+            }
+        }
+
         return rootView
     }
 }
