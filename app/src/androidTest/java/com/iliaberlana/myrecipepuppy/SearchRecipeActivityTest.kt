6e10c727e4787a61d0296464e849a258975e7647
Ilia <ilia.berlana@gmail.com>
2019-07-22 14:39:12 +0200

    Show a welcome message (tested)

--- app/src/androidTest/java/com/iliaberlana/myrecipepuppy/SearchRecipeActivityTest.kt
+++ app/src/androidTest/java/com/iliaberlana/myrecipepuppy/SearchRecipeActivityTest.kt
@@ -3,15 +3,13 @@ package com.iliaberlana.myrecipepuppy
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnitRunner
 import com.iliaberlana.myrecipepuppy.ui.search.SearchRecipesActivity
 import okhttp3.mockwebserver.MockWebServer
-import org.hamcrest.CoreMatchers.not
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -49,7 +47,10 @@ class SearchRecipeActivityTest: AndroidJUnitRunner() {
         activityRule.launchActivity(null)
 
         Espresso.onView(withId(R.id.recipes_texterror))
-            .check(ViewAssertions.matches(not(isDisplayed())))
+            .check(ViewAssertions.matches(isDisplayed()))
+
+        Espresso.onView(withText(R.string.welcome))
+            .check(ViewAssertions.matches(isDisplayed()))
     }
 
     /*override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application {
